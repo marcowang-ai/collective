715f555c5db945841fffc419772c63facb4b125e
Marco Wang <marcowang@gmail.com>
2025-09-11 16:32:56 -0500

    Fix fetch syntax error in redemption page

--- server.js
+++ server.js
@@ -914,7 +914,7 @@ async function redeem() {
   btn.textContent = 'Processing...';
 
   try {
-    const response = await fetch
+    const response = await fetch(\`/redeem/\${selectedVendor}/\${benefitKey}\`, {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(body)
